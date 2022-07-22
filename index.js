7b3948490d4cdb99aa38d9eb70db71e6e75c5a93
Gavin Anderson <itsbigchamp@Gavins-MacBook-Air.local>
2022-07-22 15:23:49 -0400

    read from firestore 3 ways

--- index.js
+++ index.js
@@ -9,6 +9,45 @@ initializeApp({
 
 const db = getFirestore()
 
-const car = {make: 'Audi', model: 'a3', year: 2018, color: 'grey'}
+// const car = {make: 'Ferrari', model: 'GTO', year: 2008, color: 'red'}
 
-db.collection('cars').add(car)
+// db.collection('cars').add(car)
+//     .then(doc => {
+//         console.log('Doc added:', doc.id)
+//     })
+//     .catch(err => console.error(err))
+    
+// db.collection('cars').doc('lambo')
+//     .set({make: 'Lamborghini', model: 'Diablo', year: 2020, color: 'yellow'})
+
+// db.collection('cars').doc('lambo')
+//     .update({model: 'Diablo', color: 'hot pink'})
+
+// Get a single document:
+db.collection('cars').doc('lambo').get()
+    .then(doc => {
+        console.log(doc.id)
+        console.log(doc.data())
+    })
+    .catch(console.error)
+
+ // Get a whole collection
+ db.collection('cars').get()
+    .then(collection => {
+        collection.docs.forEach(doc => console.log(doc.data()))
+    })
+    .catch(console.error)
+
+    // Query docs from collection:
+db.collection('cars')
+    .where('year', '>=', 2015)
+    .get()
+        .then(collection => {
+            const cars = collection.docs.map(doc => {
+                let car = doc.data()
+                car.id = doc.id
+                return car
+            })
+            console.log(cars)
+        })
+        .catch(console.error)
